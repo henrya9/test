6cebdfec31401cef3682d5fb035608e730a50c4b
aehen <aehen@LAPTOP-TUUAMD1E>
2022-10-05 13:39:01 -0400

    First Commit

--- src/syst17796/Syst17796.java
+++ src/syst17796/Syst17796.java
@@ -12,7 +12,7 @@ public class Syst17796 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        System.out.println("Hello world!");
     }
 
 }
